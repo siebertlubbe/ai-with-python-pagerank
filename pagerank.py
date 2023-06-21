fad3ec2e7a5dad6470cdc6c8791e15d6db611bf2
Siebert Lubbe <siebertlubbe@Sieberts-MacBook-Pro.local>
2023-06-21 13:49:35 +1000

    Implemenation and tests for transition_model and sample_pagerank

--- pagerank.py
+++ pagerank.py
@@ -57,7 +57,24 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+
+    # Create a dictionary to store the probability distribution
+    probability_distribution = {}
+
+    # If the page has no outgoing links, then transition_model should return a probability distribution that chooses randomly among all pages with equal probability.
+    if len(corpus[page]) == 0:
+        for page_ in corpus:
+            probability_distribution[page_] = 1 / len(corpus)
+        return probability_distribution
+    
+    # With probability damping_factor, the random surfer should randomly choose one of the links from page with equal probability.
+    for corpus_page in corpus:
+        probability_distribution[corpus_page] = (1 - damping_factor) / len(corpus)
+    for link in corpus[page]:
+        probability_distribution[link] += damping_factor / len(corpus[page])
+    for corpus_page in corpus:
+        probability_distribution[corpus_page] = round(probability_distribution[corpus_page], 4)
+    return probability_distribution
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +86,29 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+    # Create a dictionary to store the number of times each page is sampled
+    page_visits = {}
+    for page in corpus:
+        page_visits[page] = 0
+    
+    # Choose a page at random to start
+    current_page = random.choice(list(corpus.keys()))
+    page_visits[current_page] = 1
+
+    # Generate n samples
+    for i in range(n - 1):
+        current_page_transition_model = transition_model(corpus, current_page, damping_factor)
+        new_page = random.choices(list(current_page_transition_model.keys()), weights=list(current_page_transition_model.values()))[0]
+        current_page = new_page
+        page_visits[current_page] += 1
+
+    # Calculate the proportion of samples for each page
+    page_rank = {}
+    for page in page_visits:
+        page_rank[page] = page_visits[page] / n
+    
+    return page_rank
 
 
 def iterate_pagerank(corpus, damping_factor):
---
+++ test_pagerank.py
@@ -0,0 +1,44 @@
+import unittest
+
+import pagerank as pr
+
+class TestPageRank(unittest.TestCase):
+
+    def test_transition_mode(self):
+        corpus = {
+            "1.html": {"2.html", "3.html"},
+            "2.html": {"3.html"},
+            "3.html": {"2.html"}
+        }
+        
+        damping_factor = 0.85
+        expected_1 = {
+            "1.html": 0.05,
+            "2.html": 0.475,
+            "3.html": 0.475
+        }
+        self.assertEqual(pr.transition_model(corpus, "1.html", damping_factor), expected_1)
+
+        expected_2 = {
+            "1.html": 0.05,
+            "2.html": 0.05,
+            "3.html": 0.9
+        }
+        self.assertEqual(pr.transition_model(corpus, "2.html", damping_factor), expected_2)
+
+    def test_sample_pagerank(self):
+        corpus = {
+            "1.html": {"2.html", "3.html"},
+            "2.html": {"3.html"},
+            "3.html": {"2.html"}
+        }
+        damping_factor = 0.85
+        n = 10000
+        pageranks = pr.sample_pagerank(corpus, damping_factor, n)
+        self.assertEqual(len(pageranks), 3)
+        self.assertAlmostEqual(pageranks["1.html"], 0.05, delta=0.01)
+        self.assertAlmostEqual(pageranks["2.html"], 0.475, delta=0.01)
+        self.assertAlmostEqual(pageranks["3.html"], 0.475, delta=0.01)
+
+if __name__ == '__main__':
+    unittest.main()
